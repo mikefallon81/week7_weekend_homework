68ae613cec7e53bdf2eb0ddd28a9b44f8de5c1a1
mikefallon <mikefallon81@gmail.com>
2019-08-10 11:56:11 +0100

    selectedArticle method and select option created

---
+++ src/components/ArticleSelect.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <select @change="articleSelected">
+    <option selected disabled></option>
+    <option v-for="(article, index) :value="index" in articles">{{article.webTitle}}</option>
+
+  </select>
+</template>
+
+<script>
+import { eventBus } from '@/main.js';
+
+
+export default {
+  name: 'article-select',
+  props: ['articles'],
+  methods: {
+    articleSelected(e) {
+      const selectedArticle = e.target.value;
+      eventBus.$emit('article-selected', selectedArticle);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>

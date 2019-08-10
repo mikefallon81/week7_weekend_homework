e735bb2798201aeb192fe16a263626b1bba0826b
mikefallon <mikefallon81@gmail.com>
2019-08-10 11:55:24 +0100

    fetch pulling in correct object

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template lang="html">
-
+  <div>
+    <article-select :articles="articles" />
+  </div>
 </template>
 
 <script>
+import ArticleSelect from '@/components/ArticleSelect.vue';
+
 export default {
+  components: {
+    'article-select': ArticleSelect
+  },
   data(){
     return{
-      stories: []
+      articles: []
     }
   },
   mounted(){
     fetch("https://content.guardianapis.com/search?q=brexit&format=json&api-key=test")
     .then(res => res.json())
-    .then(stories => this.stories = stories)
+    .then(articles => this.articles = articles.response.results)
   }
 }
 </script>
